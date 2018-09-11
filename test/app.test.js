2155fb29d23a9e73e595daad22d2e8df96f8fdea
canderson22 <charles.anderson.dev@gmail.com>
2018-09-10 20:25:05 -0700

    refactor of test

--- test/app.test.js
+++ test/app.test.js
@@ -7,34 +7,40 @@ afterAll(() => {
 });
 
 describe('Test the root path', () => {
-  test('It should response the GET method', done => {
-    request(app)
-      .get('/')
-      .then(response => {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
-  });
+  test(
+    'It should response the GET method',
+    async done => {
+      await request(app)
+        .get('/')
+        .then(response => {
+          expect(response.statusCode).toBe(200);
+          done();
+        });
+    },
+    10000
+  );
 });
 
 describe('Test restful api', () => {
-  test('It should respond with data length equal to 4', done => {
-    request(app)
-      .get('/api')
-      .then(res => res.body)
-      .then(data => {
-        expect(data.length).toBe(4);
-        done();
-      });
-  });
+  test(
+    'It should respond with data length equal to 4',
+    async () => {
+      await request(app)
+        .get('/api')
+        .then(res => res.body)
+        .then(data => {
+          expect(data.length).toBe(4);
+        });
+    },
+    10000
+  );
 
-  test('It should respond with an array of objects', done => {
-    request(app)
+  test('It should respond with an array of objects', async () => {
+    await request(app)
       .get('/api')
       .then(res => res.body)
       .then(data => {
         expect(Array.isArray(data)).toBe(true);
-        done();
-      });
+      }, 10000);
   });
 });

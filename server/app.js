fa6e4c46583c96ee4950c17a2a5b689596079166
canderson22 <charles.anderson.dev@gmail.com>
2018-09-10 19:33:09 -0700

    forgot to add mongo urlParser

--- server/app.js
+++ server/app.js
@@ -1,24 +1,25 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-require("dotenv").config();
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const logger = require("morgan");
-const routes = require("../routes");
+require('dotenv').config();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const routes = require('../routes');
 
 mongoose.connect(
   process.env.MONGODB_URI,
+  { useNewUrlParser: true },
   err => {
     console.log(err || `MongoDB connected`);
   }
 );
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(logger("dev"));
+app.use(logger('dev'));
 
-app.use("/api", routes);
+app.use('/api', routes);
 
 module.exports = {
   app,

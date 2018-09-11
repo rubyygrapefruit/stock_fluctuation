c16bee498ae63f3d4bf032c74bc23eddf2b1a312
canderson22 <charles.anderson.dev@gmail.com>
2018-09-11 14:06:48 -0700

    created header for component

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,15 @@
 /* global document */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './App.css';
+
+//Components
+import Header from './components/header/Header.jsx';
 
 const App = () => (
-  <div>
-    <h1>hello world!</h1>
+  <div className="uk-container">
+    <Header />
+    <h1 className="uk-heading-primary">hello world!</h1>
   </div>
 );
 

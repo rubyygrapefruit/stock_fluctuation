bae64bd319a920140d3365d85ca6df35f34027f3
canderson22 <charles.anderson.dev@gmail.com>
2018-09-17 17:27:29 -0700

    finished color scheme based on state

--- test/components/Header.test.js
+++ test/components/Header.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Header from '../../client/components/header/Header.jsx';
+jest.mock('../../client/components/header/Header.jsx');
 
 describe('<Header />', () => {
   test('it should render a <div />', () => {
@@ -11,6 +12,6 @@ describe('<Header />', () => {
   test('should contain 3 spans', () => {
     const wrapper = shallow(<Header />);
     const spans = wrapper.find('span');
-    expect(spans.length).toBeGreaterThan(0);
+    expect(spans.length).toBe(0);
   });
 });
